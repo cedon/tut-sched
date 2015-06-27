61e6348d61dbfde569ca7711987bd9007b6032ea
Jon Liebold <jliebold@outlook.com>
2015-06-26 20:26:01 -0400

    Created development folder structure

--- _js/columns.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-$(document).ready(function() {
-   // Make menu in columns since IE versions older than 10 don't support it
-   $(".two-cols").columnize({ columns: 2 });
-   $(".three-cols").columnize({ columns: 3 });
-});
