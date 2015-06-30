664d69094a48a84f45b3b796bdc89a8d8545e189
Jon Liebold <jliebold@outlook.com>
2015-06-30 11:26:38 -0400

    Added JS toggle functions and aria-hidden attribute

---
+++ Builds/dev/_js/testing.js
@@ -0,0 +1,41 @@
+var buttonLinks = document.querySelectorAll('.button a');
+var subjectDivs = document.querySelectorAll('.schedsubject');
+var theNewID,
+    theID;
+
+[].forEach.call( buttonLinks, function(el){
+    el.setAttribute('href', '#');
+
+
+    //console.log(theNewID);
+
+    el.addEventListener('click', function() {
+        [].forEach.call(subjectDivs, function(div) {
+            theID = el.parentNode.getAttribute('id');
+            var regEx = '_button';
+            theNewID = theID.replace(regEx, '');
+            subjectID = div.getAttribute('id');
+            if (subjectID == theNewID) {
+                div.style.display = 'block';
+            } else {
+                div.style.display = 'none';
+            }
+        });
+    }, false);
+});
+
+[].forEach.call(buttonLinks, function(el) {
+    el.addEventListener('click', function() {
+        [].forEach.call(subjectDivs, function(div) {
+            theID = el.parentNode.getAttribute('id');
+            var regEx = '_button';
+            theNewID = theID.replace(regEx, '');
+            subjectID = div.getAttribute('id');
+            if (subjectID == theNewID) {
+                div.setAttribute("aria-hidden", "false");
+            } else {
+                div.setAttribute("aria-hidden", "true");
+            }
+        });
+    }, false);
+});
