460f7f99de80fe2f8ae4cd552eb8ca4fde4089a5
Jon Liebold <jliebold@outlook.com>
2015-08-20 22:29:27 -0400

    Added more to pagination script

--- Src/scripts/pagination.js
+++ Src/scripts/pagination.js
@@ -1,13 +1,21 @@
 var theList = document.getElementById("navlinks");
+var theNextSibling = document.querySelector("#navlinks").parentNode.nextElementSibling;
 var theListParentNode = theList.parentElement.nodeName;
 var theListParentNodeId = theList.parentNode.getAttribute("id");
 var theListParent = document.getElementById(theListParentNodeId);
 var theListItems = theList.getElementsByTagName("li");
+var theListItemLinks = theList.getElementsByTagName("a");
 var listCount = theListItems.length;
+var firstColCount, secColCount, i;
 
 // Assuming Two Columns
-secColCount = Math.floor(listCount / 2);
-firstColCount = listCount - secColCount;
+if ( listCount % 2 == 1) {
+    secColCount = Math.floor(listCount / 2);
+    firstColCount = listCount - secColCount;
+} else {
+    firstColCount = listCount / 2;
+    secColCount = listCount / 2;
+}
 
 console.log("The List's Parent is: " + theListParentNode);
 console.log("The List's Parent ID is: " + theListParentNodeId);
@@ -21,13 +29,26 @@ theList.parentNode.removeChild(theList);
 // Create the First Column and Populate It
 var firstColDiv = document.createElement("div");
 firstColDiv.id = "firstCol";
+firstColDiv.style.float = "left";
+firstColDiv.style.width = "50%";
 var firstColDivList = document.createElement("ul");
 
-
 for (i = 0; i < firstColCount; i++) {
+    // Create the new li element
     var theNewItem = document.createElement("li");
-    var theNewItemText = document.createTextNode(theListItems[i].innerHTML);
-    theNewItem.appendChild(theNewItemText);
+    theNewItem.id = theListItemLinks[i].parentNode.getAttribute("id");
+    theNewItem.className = "button";
+
+    // Create the new a element
+    var theNewItemLink = document.createElement("a");
+    theNewItemLink.setAttribute("href", "#");
+
+    //Create new text node
+    var theNewItemText = document.createTextNode(theListItemLinks[i].innerHTML);
+
+    // Link the new elements together
+    theNewItemLink.appendChild(theNewItemText);
+    theNewItem.appendChild(theNewItemLink);
     firstColDivList.appendChild(theNewItem);
 }
 firstColDiv.appendChild(firstColDivList);
@@ -39,11 +60,26 @@ secColDiv.id = "secCol";
 var secColDivList = document.createElement("ul");
 
 
-for (i = listCount - (firstColCount - 1); i < listCount; i++) {
+for (i = listCount - secColCount; i < listCount; i++) {
+    // Create the new li element
     var theNewItem = document.createElement("li");
-    var theNewItemText = document.createTextNode(theListItems[i].innerHTML);
-    theNewItem.appendChild(theNewItemText);
+    theNewItem.id = theListItemLinks[i].parentNode.getAttribute("id");
+    theNewItem.className = "button";
+
+    // Create the new a element
+    var theNewItemLink = document.createElement("a");
+    theNewItemLink.setAttribute("href", "#");
+
+    //Create new text node
+    var theNewItemText = document.createTextNode(theListItemLinks[i].innerHTML);
+
+    // Link the new elements together
+    theNewItemLink.appendChild(theNewItemText);
+    theNewItem.appendChild(theNewItemLink);
     secColDivList.appendChild(theNewItem);
 }
 secColDiv.appendChild(secColDivList);
 theListParent.appendChild(secColDiv);
+
+// Clear the next Div after columns
+theNextSibling.style.clear = "both";a
