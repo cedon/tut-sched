18615f8a0055df850ecd7cbc03ec0f4a0fd21010
Jon Liebold <jliebold@outlook.com>
2015-08-21 19:34:42 -0400

    Finished basic subject toggle function and onclick event handler

---
+++ Src/javascript/sched_toggle.js
@@ -0,0 +1,20 @@
+var theButtons = document.getElementsByClassName("button");
+var theSubjectDivs = document.getElementsByClassName("schedsubject");
+
+function bttnToggle() {
+    var regEx = '_button';
+    var theTargetId = this.id.replace(regEx, '');
+    for (d=0; d < theSubjectDivs.length; d++) {
+        if (theSubjectDivs[d].id === theTargetId) {
+            theSubjectDivs[d].style.display = "block";
+            theSubjectDivs[d].setAttribute("aria-hidden", "false");
+        } else {
+            theSubjectDivs[d].style.display = "none";
+            theSubjectDivs[d].setAttribute("aria-hidden", "true");
+        }
+    }
+}
+
+for (i=0; i < theButtons.length; i++) {
+    theButtons[i].addEventListener('click', bttnToggle, false);
+}
